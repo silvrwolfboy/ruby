eab6cb43abb262f8f2d90566501e296a35b639f3
Aman Gupta <aman@tmm1.net>
2014-06-10 23:39:26 -0700

    revert method cache patch

--- gc.c
+++ gc.c
@@ -1577,7 +1577,6 @@ obj_free(rb_objspace_t *objspace, VALUE obj)
 	}
 	rb_class_remove_from_module_subclasses(obj);
 	rb_class_remove_from_super_subclasses(obj);
-	rb_method_cache_clear(obj);
 	if (RANY(obj)->as.klass.ptr)
 	    xfree(RANY(obj)->as.klass.ptr);
 	RANY(obj)->as.klass.ptr = NULL;
@@ -1648,7 +1647,6 @@ obj_free(rb_objspace_t *objspace, VALUE obj)
 	}
 	rb_class_remove_from_module_subclasses(obj);
 	rb_class_remove_from_super_subclasses(obj);
-	rb_method_cache_clear(obj);
 	xfree(RANY(obj)->as.klass.ptr);
 	RANY(obj)->as.klass.ptr = NULL;
 	break;
