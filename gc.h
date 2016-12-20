1749d5b2cdc6930c618e2d40a265870e448f70ee
Aaron Patterson <aaron.patterson@gmail.com>
2016-12-20 09:14:53 -0800

    Add a "permanently non-moving" table
    This is to tag objects as "will not move for the life of the object".
    Ideally we should remove this table, but we will use it for objects
    whose object_id has been accessed.

--- gc.h
+++ gc.h
@@ -99,7 +99,9 @@ void rb_objspace_reachable_objects_from_root(void (func)(const char *category, V
 int rb_objspace_markable_object_p(VALUE obj);
 int rb_objspace_internal_object_p(VALUE obj);
 int rb_objspace_marked_object_p(VALUE obj);
+int rb_objspace_pinned_object_p(VALUE obj);
 int rb_objspace_garbage_object_p(VALUE obj);
+VALUE rb_objspace_set_non_moving(VALUE obj);
 
 void rb_objspace_each_objects(
     int (*callback)(void *start, void *end, size_t stride, void *data),
