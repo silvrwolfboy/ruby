e562312370c5a9e16a8712d179cb4b7ebea9838a
John Hawthorn <john@hawthorn.email>
2019-11-01 09:35:33 -0700

    Remove some tests

--- test/ruby/test_gc_compact.rb
+++ test/ruby/test_gc_compact.rb
@@ -72,58 +72,6 @@ class TestGCCompact < Test::Unit::TestCase
     flunk "Couldn't get objects to move"
   end
 
-  def test_find_collided_object
-    skip "figure out how to guarantee move"
-
-    list_of_objects, addresses, new_tenant = try_to_move_objects
-
-    # This is the object that used to be in new_object's position
-    loc = memory_location(new_tenant)
-    assert loc, "should have a memory location"
-
-    if (ENV['TRAVIS'] && RUBY_PLATFORM =~ /darwin/)
-      skip "tests are failing on Travis osx / Wercker from here"
-    end
-
-    address_idx = addresses.index(loc)
-    assert address_idx, "should have an address index"
-
-    previous_tenant = list_of_objects[address_idx]
-    assert previous_tenant, "should have a previous tenant"
-
-    assert_not_equal previous_tenant.object_id, new_tenant.object_id
-
-    # Should be able to look up object by object_id
-    assert_equal new_tenant, ObjectSpace._id2ref(new_tenant.object_id)
-
-    # Should be able to look up object by object_id
-    assert_equal previous_tenant, ObjectSpace._id2ref(previous_tenant.object_id)
-
-    int = (new_tenant.object_id >> 1)
-    # These two should be the same! but they are not :(
-    assert_equal int, ObjectSpace._id2ref(int.object_id)
-  end
-
-  def test_many_collisions
-    list_of_objects = big_list
-    ids       = list_of_objects.map(&:object_id)
-    addresses = list_of_objects.map(&self.:memory_location)
-
-    GC.verify_compaction_references(toward: :empty)
-
-    skip "time consuming"
-
-    new_tenants = 10.times.map {
-      find_object_in_recycled_slot(addresses)
-    }
-
-    collisions = GC.stat(:object_id_collisions)
-    skip "couldn't get objects to collide" if collisions == 0
-    assert_operator collisions, :>, 0
-    ids.clear
-    new_tenants.clear
-  end
-
   def test_complex_hash_keys
     list_of_objects = big_list
     hash = list_of_objects.hash
